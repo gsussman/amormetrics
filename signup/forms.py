8b87564b39ef87e59e91d9004797b4defa6d55de
Gene Sussman <gene.sussman@gmail.com>
2015-12-31 02:01:40 -0500

    changed color on email signup

--- signup/forms.py
+++ signup/forms.py
@@ -7,8 +7,8 @@ class EmailSignupForm(forms.ModelForm):
     class Meta:
         model = EmailSignup
         widgets = {
-            'first_name' : forms.TextInput(attrs = {'placeholder': 'First Name', 'size': '15'}),
-            'last_name'  : forms.TextInput(attrs = {'placeholder': 'Last Name', 'size': '15'}),
-            'email'  	 : forms.TextInput(attrs = {'placeholder': 'Email', 'size': '20'}),
+            'first_name' : forms.TextInput(attrs = {'placeholder': 'First Name', 'size': '15', 'style': 'color:black'}),
+            'last_name'  : forms.TextInput(attrs = {'placeholder': 'Last Name', 'size': '15', 'style': 'color:black'}),
+            'email'  	 : forms.TextInput(attrs = {'placeholder': 'Email', 'size': '20', 'style': 'color:black'}),
         }
         fields = ('first_name', 'last_name','email')
