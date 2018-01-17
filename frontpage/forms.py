458185bf91005379e96d521a69b0eeea328d2acc
Gene Sussman <gene.sussman@yale.edu>
2018-01-17 22:32:44 +0000

    remove unused form

--- frontpage/forms.py
+++ frontpage/forms.py
@@ -20,7 +20,7 @@ class ProfileForm(forms.Form):
 class FileFieldForm(forms.Form):
     file_field = forms.FileField(widget=forms.ClearableFileInput(attrs={'multiple': True}))
     
-class ImageForm1(forms.ModelForm):
-    class Meta:
-        model = Image
-        fields = ['image']
+#class ImageForm1(forms.ModelForm):
+#    class Meta:
+#        model = Image
+#        fields = ['image']
